a97f961440c36a0a21907d98613b24ae588cb2e9
liZhi <lzlz1996@126.com>
2019-04-28 13:48:37 +0800

    simple spring cloud gateway demo

---
+++ spring-cloud-simple-gateway/src/main/java/com/springcloud/springcloudsimplegateway/SpringCloudSimpleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.springcloudsimplegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSimpleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSimpleGatewayApplication.class, args);
+	}
+
+}
